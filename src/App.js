7627fb2374136eda2a957b623e8cc30ef99b3345
Jovohkir <107384933+Jovohkir@users.noreply.github.com>
2022-06-29 16:58:37 +0500

    sss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import NavbarPage from "./navbar/NavbarPage";
 import { BrowserRouter } from "react-router-dom";
 import RouterApp from "./routerApp/RouterApp";
-import SideBar from "./sidebar/SideBar";
+
 export default function App() {
   return (
     <>
--- src/navigatePage/LogIn.js
+++ src/navigatePage/LogIn.js
@@ -1,6 +1,54 @@
+import { Link } from "@mui/material";
 import React from "react";
 
 const LogIn = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="container">
+        <form>
+          <h3 className="text-center">Log In</h3>
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="mb-3">
+            <div className="custom-control custom-checkbox">
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+              />
+              <label
+                className="custom-control-label text-center"
+                htmlFor="customCheck1"
+              >
+                Remember me
+              </label>
+            </div>
+          </div>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            Forgot <Link>password ?</Link>
+          </p>
+        </form>
+      </div>
+    </>
+  );
 };
 export default LogIn;
