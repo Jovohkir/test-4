4ff45fb9089c4902f920bf4ab52e47dc548b7d6f
Jovohkir <107384933+Jovohkir@users.noreply.github.com>
2022-06-30 17:58:46 +0500

    3006

--- src/navigatePage/LogIn.js
+++ src/navigatePage/LogIn.js
@@ -15,40 +15,25 @@ const LogIn = () => {
         }}
       >
         <form>
-          <h3 className="text-center">Log In</h3>
-          <div className="mb-3">
-            <label>Email address</label>
+          <h2 className="text-center">Login</h2>
+          <div className="mb-4">
             <input
               type="email"
               className="form-control"
-              placeholder="Enter email"
+              placeholder="Enter Login"
+              required
             />
           </div>
-          <div className="mb-3">
-            <label>Password</label>
+          <div className="mb-4">
             <input
               type="password"
               className="form-control"
               placeholder="Enter password"
+              required
             />
           </div>
-          <div className="mb-3">
-            <div className="custom-control custom-checkbox">
-              <input
-                type="checkbox"
-                className="custom-control-input"
-                id="customCheck1"
-              />
-              <label
-                className="custom-control-label text-center"
-                htmlFor="customCheck1"
-              >
-                Remember me
-              </label>
-            </div>
-          </div>
           <div className="d-grid">
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary my-2">
               Submit
             </button>
           </div>
